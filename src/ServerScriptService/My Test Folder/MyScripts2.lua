78109076c76207981045d6e9f15a09fdc50f6d98
Matt Hill <matt.hill.email@gmail.com>
2020-11-01 09:37:00 -0800

    asfd

---
+++ src/ServerScriptService/My Test Folder/MyScripts2.lua
@@ -0,0 +1,12 @@
+local my_functions = {}
+
+function my_functions.foo()
+    print("Foo---------test!")
+end
+
+function my_functions.bar()
+    print("Bar!---------------test")
+end
+
+-- ModuleScripts must return exactly one value
+return my_functions
