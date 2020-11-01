ea0ef5f343fc691588f09af93f4e9ef12b762daa
Matt Hill <matt.hill.email@gmail.com>
2020-10-31 23:33:46 -0700

    asdf

--- src/ServerScriptService/server.server.lua
+++ src/ServerScriptService/server.server.lua
@@ -1,8 +1,14 @@
-local module = {
+-- Tables are store multiple values in one variable
+local my_functions = {}
+    
+-- Add a few functions to the table
+function my_functions.foo()
+    print("Foo111!")
+end
 
+function my_functions.bar()
+    print("Bar!111")
+end
 
-
-}
-
-
-return module
+-- ModuleScripts must return exactly one value
+return my_functions
