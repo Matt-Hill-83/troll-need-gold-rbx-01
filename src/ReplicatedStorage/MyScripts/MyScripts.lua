e70ef698141bbe6be3aad4db18f3989e1c0c0cce
Matt Hill <matt.hill.email@gmail.com>
2020-11-02 09:30:52 -0800

    sadf

--- src/ReplicatedStorage/MyScripts/MyScripts.lua
+++ src/ReplicatedStorage/MyScripts/MyScripts.lua
@@ -2,12 +2,16 @@ local my_functions = {}
 print('did you get this')
 
 function my_functions.foo()
+
     print("Foo---------2!")
+    print("Foo---------2!")
+    print("Foo---------2!")
+    print("Foo---------2!")
+    print("Foo---------2!")
+
 end
 
-    function my_functions.bar()
-    print("Bar!---------------111")
-end
+function my_functions.bar() print("Bar!---------------111") end
 
 -- ModuleScripts must return exactly one value
 return my_functions
--- src/ServerScriptService/MyTestFolder/MyScripts2.lua
+++ src/ServerScriptService/MyTestFolder/MyScripts2.lua
@@ -3,16 +3,10 @@ print('mysss')
 
 function my_functions.foo(part)
     print("Foo---------test!")
-    local newPart = Instance.new("Part", part) 
+    local newPart = Instance.new("Part", part)
     return newPart
 
-
 end
-
-
-
-
-
 
 -- ModuleScripts must return exactly one value
 return my_functions
