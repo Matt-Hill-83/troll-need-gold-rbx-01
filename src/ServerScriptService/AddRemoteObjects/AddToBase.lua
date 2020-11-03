f801da1a01b1eb1464050a8105532cfef333fb78
Matt Hill <matt.hill.email@gmail.com>
2020-11-03 00:43:38 -0800

    scenes positioned correctly

--- src/ServerScriptService/AddRemoteObjects/AddToBase.lua
+++ src/ServerScriptService/AddRemoteObjects/AddToBase.lua
@@ -13,24 +13,28 @@ function module.addRemoteObjects(part)
 
     for i, scene in ipairs(scenes) do
         local sceneWidth = 10
-        local sceneHeight = 30
-        local yIncrement = sceneWidth * 0.1
-        local yPosition = sceneWidth + yIncrement
+        local sceneHeight = 15
+        local zIncrement = sceneWidth * 0.2
+        local zPositionStart = 10
+        local zPosition = zPositionStart + (i - 1) * (sceneWidth + zIncrement)
+
+        local xPosition = -5
+        local yPosition = 10
 
         local newScene = Instance.new("Part", part)
         local name = scene['name']
         print(name)
 
-        local newLabel = Instance.new("TextLabel", part)
+        newScene.Size = Vector3.new(2, sceneHeight, sceneWidth)
+        newScene.Position = Vector3.new(xPosition, yPosition, zPosition)
+        newScene.Anchored = true
+        newScene.BrickColor = BrickColor.new(scene.color)
+
+        local newLabel = Instance.new("TextLabel", newScene)
 
         newLabel.Size = UDim2.new(0, 10, 0, 10)
         newLabel.Position = UDim2.new(0, 0, 0, 0)
         newLabel.Text = name
-
-        newScene.Size = Vector3.new(2, sceneHeight, sceneWidth)
-        newScene.Position = Vector3.new(-5, 0, i * yPosition)
-        newScene.Anchored = true
-        newScene.BrickColor = BrickColor.new(scene.color)
         -- newScene.BrickColor = scene.color
 
     end
