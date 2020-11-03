7be4e7e8da0f798e0ea748d4cfbeaa9e8d653546
Matt Hill <matt.hill.email@gmail.com>
2020-11-02 23:08:44 -0800

    works

--- src/ServerScriptService/AddRemoteObjects/AddToBase.lua
+++ src/ServerScriptService/AddRemoteObjects/AddToBase.lua
@@ -4,24 +4,17 @@ local HttpService = game:GetService("HttpService")
 
 local my_functions = {}
 
-local a = Sss.Source.AddRemoteObjects
-local b = a.AddToBase2
-
--- local test = require(Sss.Source.test)
 local test2 = require(Sss.Source.AddRemoteObjects.AddToBase2)
 local json = test2.getScenes()
 
 print('test001-----------------')
-print(a)
-print(b)
 print(json)
-print('test001-----------------')
+print('test002-----------------')
 
 function my_functions.addRemoteObjects(part)
     local newPart = Instance.new("Part", part)
 
     local characterStats = HttpService:JSONDecode(json)
-    -- local characterStats = HttpService:JSONDecode(jsonData)
     print(characterStats.money)
 
     return newPart
--- src/ServerScriptService/AddRemoteObjects/AddToBase2.lua
+++ src/ServerScriptService/AddRemoteObjects/AddToBase2.lua
@@ -2,15 +2,6 @@ local module2 = {hi = "5"}
 local myJson =
     '{"awards":{"died20":false,"kills10":true},"title":"1337 h4x04","is_epic":true,"money":123456}'
 
-module2.json = myJson
-
-function module2.getScenes(part)
-    local newPart = Instance.new("Part", part)
-
-    -- return newPart
-    return myJson
-end
-
--- local myJson = '{ { "character": "kat", "text": "Hi Liz!" } }'
+function module2.getScenes(part) return myJson end
 
 return module2
