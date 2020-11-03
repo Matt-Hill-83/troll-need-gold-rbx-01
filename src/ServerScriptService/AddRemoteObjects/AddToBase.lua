0d4fdbec96a7139feb34af8e51ce2f78cbe9dd03
Matt Hill <matt.hill.email@gmail.com>
2020-11-02 23:30:50 -0800

    labels attach to base

--- src/ServerScriptService/AddRemoteObjects/AddToBase.lua
+++ src/ServerScriptService/AddRemoteObjects/AddToBase.lua
@@ -1,24 +1,33 @@
+local module = {}
 print('mysss2')
 local Sss = game:GetService("ServerScriptService")
-local HttpService = game:GetService("HttpService")
 
-local my_functions = {}
-
-local test2 = require(Sss.Source.AddRemoteObjects.AddToBase2)
-local json = test2.getScenes()
+local config = require(Sss.Source.AddRemoteObjects.AddToBase2)
+local scenes = config.getScenes()
 
 print('test001-----------------')
-print(json)
+print(scenes)
 print('test002-----------------')
 
-function my_functions.addRemoteObjects(part)
-    local newPart = Instance.new("Part", part)
+function module.addRemoteObjects(part)
 
-    local characterStats = HttpService:JSONDecode(json)
-    print(characterStats.money)
+    for i, scene in ipairs(scenes) do
+        local newScene = Instance.new("Part", part)
+        local newLabel = Instance.new("TextLabel", part)
+        newLabel.Size = UDim2.new(0, 800, 0, 100)
+        newLabel.Position = UDim2.new(0, 100, 0, i * 150)
 
-    return newPart
+        newScene.Parent = part
+        local name = scene['name']
+        print(name)
+
+        newScene.Size = Vector3.new(100, 800, 100)
+        -- newScene.Position = UDim2.new(0, 100, 0, i * 150)
+
+    end
+
+    return part
 end
 
-return my_functions
+return module
 
--- src/ServerScriptService/AddRemoteObjects/AddToBase2.lua
+++ src/ServerScriptService/AddRemoteObjects/AddToBase2.lua
@@ -2,6 +2,16 @@ local module2 = {hi = "5"}
 local myJson =
     '{"awards":{"died20":false,"kills10":true},"title":"1337 h4x04","is_epic":true,"money":123456}'
 
-function module2.getScenes(part) return myJson end
+function module2.getScenes()
+
+    local char01 = {name = "Britta"}
+    local frame01 = {characters = {char01, char01}}
+    local frames = {frame01, frame01}
+    local scene01 = {name = "log", frames = {frames}}
+    local scenes = {scene01, scene01}
+
+    return scenes
+
+end
 
 return module2
