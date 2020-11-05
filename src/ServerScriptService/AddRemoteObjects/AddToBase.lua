72fb30af375ddb4b1a2023143ad3cad7a125f995
Matt Hill <matt.hill.email@gmail.com>
2020-11-05 02:20:36 -0800

    extract createRowOfParts2

--- src/ServerScriptService/AddRemoteObjects/AddToBase.lua
+++ src/ServerScriptService/AddRemoteObjects/AddToBase.lua
@@ -2,12 +2,13 @@ local module = {}
 print('mysss2')
 local Sss = game:GetService("ServerScriptService")
 
-local config = require(Sss.Source.AddRemoteObjects.ScenesConfig)
+local SceneConfig = require(Sss.Source.AddRemoteObjects.ScenesConfig)
 local Dialog = require(Sss.Source.AddRemoteObjects.Dialog)
 local Part = require(Sss.Source.AddRemoteObjects.Part)
 local RowOfParts = require(Sss.Source.AddRemoteObjects.RowOfParts)
+local RowOfParts2 = require(Sss.Source.AddRemoteObjects.RowOfParts2)
 
-local sceneConfigs = config.getScenesConfig()
+local sceneConfigs = SceneConfig.getScenesConfig()
 local sceneDepth = 2
 
 createNewPart = function(item, size)
@@ -54,69 +55,27 @@ renderItems = function(parent, items)
 end
 
 function module.addRemoteObjects(part)
-    local basePadding = 1
-    local sceneProps = {
-        size = {x = 4, y = 4, z = sceneDepth},
-        partName = "Scene"
-    }
 
-    local frameIndex = 1
+    local basePadding = 1
 
     local rowProps = {
         xGap = 2,
         xOffset = basePadding,
         zOffset = basePadding,
 
-        createNewItemFunc = createNewPart,
+        -- createNewItemFunc = createNewPart,
         parent = part,
         direction = -1,
         moveTowardZero = {x = -1, y = 1, z = -1}
     }
 
-    local edgePropsX = {part = part, axis = "X"}
-    local parentEdgeX = RowOfParts.getPartFarEdge(edgePropsX)
+    local props = {
+        rowProps = rowProps,
+        sceneConfigs = sceneConfigs
+        --
+    }
 
-    local edgePropsY = {part = part, axis = "Y"}
-    local parentEdgeY = RowOfParts.getPartFarEdge(edgePropsY)
-
-    local edgePropsZ = {part = part, axis = "Z"}
-    local parentEdgeZ = RowOfParts.getPartFarEdge(edgePropsZ)
-
-    local sceneWidth = sceneProps.size.x
-    local xIncrement = rowProps.direction * (sceneWidth + rowProps.xGap)
-
-    local prevX = parentEdgeX - rowProps.xOffset
-    local y = parentEdgeY + rowProps.xOffset
-    local z = parentEdgeZ - rowProps.xOffset
-
-    for i, sceneConfig in ipairs(sceneConfigs) do
-        local x = prevX
-
-        local position = {x = x, y = y, z = z}
-
-        local adjustmentProps = {
-            size = sceneProps.size,
-            position = position,
-            moveTowardZero = rowProps.moveTowardZero
-        }
-        local edgeAdjustedPosition = Part.getEdgePositionFromCenter(
-                                         adjustmentProps)
-
-        local newPartProps = {
-            decalId = sceneConfig.decalId,
-            size = sceneProps.size,
-            name = sceneProps.partName .. "-" .. i,
-            position = edgeAdjustedPosition,
-            parent = rowProps.parent
-        }
-        local newPart = Part.createPart2(newPartProps)
-
-        print('x' .. ' - start');
-        print(x);
-        print('x' .. ' - end');
-
-        prevX = x + xIncrement
-    end
+    local renderedScenes = RowOfParts2.createRowOfParts2(props)
 
     -- local scene = sceneConfigs[i]
     -- local characters = scene.frames[frameIndex].characters
--- src/ServerScriptService/AddRemoteObjects/RowOfParts2.lua
+++ src/ServerScriptService/AddRemoteObjects/RowOfParts2.lua
@@ -1,3 +1,6 @@
+local Sss = game:GetService("ServerScriptService")
+local Part = require(Sss.Source.AddRemoteObjects.Part)
+
 local module = {}
 
 function getParentFarEdge(props)
@@ -23,39 +26,62 @@ function getPartFarEdge(props)
     return partFarEdge
 end
 
-
-
 function createRowOfParts2(props)
-    
+    local rowProps = props.rowProps
+    local sceneConfigs = props.sceneConfigs
 
+    local sceneDepth = 2
 
+    local sceneProps = {
+        size = {x = 4, y = 4, z = sceneDepth},
+        partName = "Scene"
+    }
 
+    local frameIndex = 1
 
+    local edgePropsX = {part = rowProps.parent, axis = "X"}
+    local parentEdgeX = getPartFarEdge(edgePropsX)
 
+    local edgePropsY = {part = rowProps.parent, axis = "Y"}
+    local parentEdgeY = getPartFarEdge(edgePropsY)
 
+    local edgePropsZ = {part = rowProps.parent, axis = "Z"}
+    local parentEdgeZ = getPartFarEdge(edgePropsZ)
 
+    local sceneWidth = sceneProps.size.x
+    local xIncrement = rowProps.direction * (sceneWidth + rowProps.xGap)
 
+    local prevX = parentEdgeX - rowProps.xOffset
+    local y = parentEdgeY + rowProps.xOffset
+    local z = parentEdgeZ - rowProps.xOffset
 
+    for i, sceneConfig in ipairs(sceneConfigs) do
+        local x = prevX
 
+        local position = {x = x, y = y, z = z}
 
+        local adjustmentProps = {
+            size = sceneProps.size,
+            position = position,
+            moveTowardZero = rowProps.moveTowardZero
+        }
+        local edgeAdjustedPosition = Part.getEdgePositionFromCenter(
+                                         adjustmentProps)
 
-    
-    local rowOfParts = {}
+        local newPartProps = {
+            decalId = sceneConfig.decalId,
+            size = sceneProps.size,
+            name = sceneProps.partName .. "-" .. i,
+            position = edgeAdjustedPosition,
+            parent = rowProps.parent
+        }
+        local newPart = Part.createPart2(newPartProps)
 
-
-        rowOfParts[i] = newItem
-
-        
-        
-        
-        
+        prevX = x + xIncrement
     end
 
-    
-    return rowOfParts
 end
 
 module.createRowOfParts2 = createRowOfParts2
-
 
 return module
