ad3861e78ad7d277fe6ce301274a842c6a1c35bf
Matt Hill <matt.hill.email@gmail.com>
2020-11-05 22:40:53 -0800

    asdf

--- src/ServerScriptService/AddRemoteObjects/Dialog.lua
+++ src/ServerScriptService/AddRemoteObjects/Dialog.lua
@@ -4,6 +4,13 @@ local Part = require(Sss.Source.AddRemoteObjects.Part)
 
 local module = {}
 
+local testDict01 = {
+    text = "aaa bbb cccc ddd eee fff ggg hhh iii jjj kkk lll mmm nnn ooo ppp qqq rrr sss ttt",
+    color = "Yellow",
+    char = "Britta"
+}
+local testDict02 = {text = "two", color = "Yellow", char = "Britta"}
+
 renderButtonBlock = function(props)
     local parent = props.parent
     local sibling = props.sibling
@@ -28,50 +35,40 @@ renderButtonBlock = function(props)
 
 end
 
-renderDialogBlock = function(props) end
-
-renderDialog = function(base)
-
-    local pixelsPerStud = 40
-    local paddingInPx = 1 * pixelsPerStud
-    local TextButtonPaddingInPx = 1 * pixelsPerStud
+renderDialogBlock = function(props)
+    local parent = props.parent
 
     local dialogBlockProps = {
         name = 'dialogBlock',
-        parent = base,
-        size = base.Size + Vector3.new(-2, -2, 0),
-        position = base.Position + Vector3.new(0, 0, -0.5),
-        color = BrickColor.new("White")
+        parent = parent,
+        size = parent.Size + Vector3.new(-2, -2, 0),
+        position = parent.Position + Vector3.new(0, 0, -0.5),
+        color = BrickColor.new("Buttermilk")
     }
 
-    local dialogBlock = Part.createPartWithVectors(dialogBlockProps)
+    return Part.createPartWithVectors(dialogBlockProps)
 
-    local parentWidth = dialogBlock.Size.X * pixelsPerStud - (2 * paddingInPx)
-    local parentHeight = dialogBlock.Size.Y * pixelsPerStud
+end
 
-    local sgui = Instance.new("SurfaceGui", dialogBlock)
-    local renderButtonBlockProps = {parent = base, sibling = dialogBlock}
+renderTexts = function(props)
+    local parent = props.parent
+    local pageNum = props.pageNum
+    local paddingInPx = props.paddingInPx
+    local pixelsPerStud = props.pixelsPerStud
 
-    renderButtonBlock(renderButtonBlockProps)
-
-    -- local dialogSurface = textButton.Parent
-    -- Instance.new("TextLabel", sgui)
-    local counter = 1
-
-    local testDict01 = {
-        text = "aaa bbb cccc ddd eee fff ggg hhh iii jjj kkk lll mmm nnn ooo ppp qqq rrr sss ttt",
-        color = "Yellow",
-        char = "Britta"
-    }
-    local testDict02 = {text = "two", color = "Yellow", char = "Britta"}
-
-    -- local texts = {testDict01}
+    local sgui = Instance.new("SurfaceGui", parent)
     local texts = {testDict01, testDict02, testDict01}
 
-    local dialogY = paddingInPx
+    local parentWidth = parent.Size.X * pixelsPerStud - (2 * paddingInPx)
+    local parentHeight = parent.Size.Y * pixelsPerStud
+
+    local bottomGap = 1
+    local dialogY = bottomGap
+
+    local textButtonPaddingInPx = 1 * pixelsPerStud
 
     for i, dialog in ipairs(texts) do
-        local charName = texts[counter]['char']
+        local charName = texts[pageNum]['char']
         local text = charName .. ": " .. dialog['text']
 
         local font = Enum.Font.Legacy
@@ -90,7 +87,7 @@ renderDialog = function(base)
         newLabel.Name = "Dialog-" .. i
         newLabel.Text = text
         newLabel.Size = UDim2.new(0, parentWidth, 0, height)
-        newLabel.Position = UDim2.new(0, TextButtonPaddingInPx, 0, dialogY)
+        newLabel.Position = UDim2.new(0, textButtonPaddingInPx, 0, dialogY)
         newLabel.Selectable = true
 
         newLabel.TextWrapped = true
@@ -103,13 +100,37 @@ renderDialog = function(base)
         dialogY = dialogY + absoluteHeight + paddingInPx
 
     end
+end
+
+renderDialog = function(base)
+
+    local dialogBlockProps = {parent = base}
+    local dialogBlock = renderDialogBlock(dialogBlockProps)
+
+    local pixelsPerStud = 40
+    local paddingInPx = 1 * pixelsPerStud
+
+    local renderButtonBlockProps = {parent = base, sibling = dialogBlock}
+    renderButtonBlock(renderButtonBlockProps)
+
+    -- local dialogSurface = textButton.Parent
+    -- Instance.new("TextLabel", sgui)
+    local pageNum = 1
+
+    local renderTextsProps = {
+        parent = dialogBlock,
+        pageNum = pageNum,
+        paddingInPx = paddingInPx,
+        pixelsPerStud = pixelsPerStud
+    }
+    renderTexts(renderTextsProps)
 
     -- local textButton = Instance.new("TextButton", sgui)
     -- textButton.Size = UDim2.new(0, childWidth, 0, childHeight)
     -- textButton.Text = "Next Page!"
     -- local function onActivated()
-    --     counter = counter + 1
-    --     textButton.Text = "Page: " .. counter
+    --     pageNum = pageNum + 1
+    --     textButton.Text = "Page: " .. pageNum
 
     -- end
 
